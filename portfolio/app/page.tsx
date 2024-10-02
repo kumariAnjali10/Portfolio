8a61cb0cc65d3f2540cf9004aef6e8e2839507fb
Kumarianjali <kumarianjali206700@gmail.com>
2024-10-02 21:53:29 +0530

    Improved the UI

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -8,7 +8,12 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome/>}
+          {name: 'Home', link: '/', icon: <FaHome/>},
+          {name: 'Projects', link: '/', icon: <FaHome/>},
+          {name: 'Experience', link: '/', icon: <FaHome/>},
+          {name: 'Contact', link: '/', icon: <FaHome/>}
+
+
         ]}/>
         <Hero/>
         <Grid/>
--- portfolio/components/Grid.tsx
+++ portfolio/components/Grid.tsx
@@ -1,23 +1,22 @@
 
+import { gridItems } from '@/data'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
 const Grid = () => {
   return (
     <section id="about">
    <BentoGrid>
-    {[
-      {title: 'Title1', description: 'Desc1',id:1},
-      {title: 'Title2', description: 'Desc2',id:2},
-      {title: 'Title3', description: 'Desc3',id:3},
-      {title: 'Title4', description: 'Desc4',id:4},
-    
-    ].map
-     ((item) => (
+    {gridItems.map(({id, title, description, className,img, imgClassName, titleClassName, spareImg   }) => (
         <BentoGridItem
-        id={item.id}
-        key={item.id}
-        title={item.title}
-        description={item.description}
+        id={id}
+        key={id}
+        title={title}
+        description={description}
+        className={className}
+        img={img}
+        imgClassName={imgClassName}
+        titleClassName= {titleClassName}
+        spareImg= {spareImg}
         />
     ))}
 
